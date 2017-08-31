2a4dad7072c6256bddeebd14a96214a6567a147f
Graham Hukill <ghukill@gmail.com>
2017-08-31 11:49:43 -0400

    first commie

---
+++ core/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. Welcome to Combine.")
