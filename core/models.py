aef5c349ca287e8ca44a2b1f6f65ad8329118ffa
Graham Hukill <ghukill@gmail.com>
2017-09-19 19:41:57 +0000

    orgs structure made

--- core/models.py
+++ core/models.py
@@ -671,6 +671,7 @@ class HarvestJob(CombineJob):
 		super().__init__(user=user)
 
 		self.record_group = record_group
+		self.organization = self.record_group.organization
 		self.oai_endpoint = oai_endpoint
 		self.overrides = overrides
 
@@ -708,7 +709,7 @@ class HarvestJob(CombineJob):
 
 		# construct harvest path
 		# harvest_save_path = '/user/combine/record_group/%s/jobs/harvest/%s' % (self.record_group.id, self.job.id)
-		harvest_save_path = '%s/record_group/%s/jobs/harvest/%s' % (settings.AVRO_STORAGE.rstrip('/'), self.record_group.id, self.job.id)
+		harvest_save_path = '%s/organizations/%s/record_group/%s/jobs/harvest/%s' % (settings.AVRO_STORAGE.rstrip('/'), self.organization.id, self.record_group.id, self.job.id)
 
 		# create shallow copy of oai_endpoint and mix in overrides
 		harvest_vars = self.oai_endpoint.__dict__.copy()
