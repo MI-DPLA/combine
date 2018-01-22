73386505a9b4237f875b81766b48bbda99093032
Graham Hukill <ghukill@gmail.com>
2018-01-22 17:05:46 +0000

    rough scaffold of Livy/Spark status

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from core.models import LivySession
 
 def combine_settings(request):
 	
@@ -11,4 +11,18 @@ def combine_settings(request):
 		'APP_HOST': settings.APP_HOST,
 		'COMBINE_OAI_ENDPOINT': settings.COMBINE_OAI_ENDPOINT,
 		'DPLA_API_KEY': settings.DPLA_API_KEY
+	}
+
+
+def livy_session(request):
+
+	'''
+	Make Livy session information available to all views
+	'''
+
+	# get active livy session
+	lv = LivySession.get_active_session()
+
+	return {
+		'LIVY_SESSION':lv
 	}
