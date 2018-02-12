1ca3c62c68df5c39273c2dc31f1fb8807792f867
Graham Hukill <ghukill@gmail.com>
2018-02-12 15:31:43 +0000

    updates for static and oai

--- combine/settings.py
+++ combine/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/combine/static'
 
 
 # Logging
