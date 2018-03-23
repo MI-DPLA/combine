8b045e0ad84d3b540c2bdfbb2965f529319b1a29
Jose Blanco <blancoj@umich.edu>
2018-03-23 20:55:05 +0000

    offer bulk delete of jobs.

--- core/urls.py
+++ core/urls.py
@@ -40,6 +40,9 @@ urlpatterns = [
 	url(r'^organization/(?P<org_id>[0-9]+)/record_group/(?P<record_group_id>[0-9]+)/job/(?P<job_id>[0-9]+)/reports/create_audit_report$', views.job_reports_create_audit, name='job_reports_create_audit'),
 	url(r'^organization/(?P<org_id>([0-9]|(DYNAMIC_ORG_ID))+)/record_group/(?P<record_group_id>([0-9]|(DYNAMIC_RG_ID))+)/job/(?P<job_id>([0-9]|(DYNAMIC_ID))+)/job_lineage_json$', views.job_lineage_json, name='job_lineage_json'),
 
+        url(r'^jobs/delete_jobs$', views.delete_jobs, name='delete_jobs'),
+        url(r'^organization/(?P<org_id>[0-9]+)/record_group/delete_jobs$', views.delete_jobs, name='delete_jobs'),
+
 	# Job Record Validation Scenarios
 	url(r'^organization/(?P<org_id>[0-9]+)/record_group/(?P<record_group_id>[0-9]+)/job/(?P<job_id>[0-9]+)/job_validation_scenario_failures/(?P<job_validation_id>[0-9]+)$', views.job_validation_scenario_failures, name='job_validation_scenario_failures'),
 
