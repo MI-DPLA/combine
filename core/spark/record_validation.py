09fb3ae70df5f5485151bec4f25d7cecbfd51bfd
Graham Hukill <ghukill@gmail.com>
2018-06-11 16:44:11 +0000

    validation refactoring

--- core/spark/record_validation.py
+++ core/spark/record_validation.py
@@ -58,6 +58,11 @@ class ValidationScenarioSpark(object):
 		self.records_df = records_df
 		self.validation_scenarios = validation_scenarios
 
+		# init logging support
+		spark.sparkContext.setLogLevel('INFO')
+		log4jLogger = spark.sparkContext._jvm.org.apache.log4j
+		self.logger = log4jLogger.LogManager.getLogger(__name__)
+
 
 	def run_record_validation_scenarios(self):
 
@@ -90,33 +95,11 @@ class ValidationScenarioSpark(object):
 
 			# schematron based validation scenario
 			if vs.validation_type == 'sch':
-
-				# run udf map
-				udf_func = self.validate_schematron_udf # must pass static method not attached to self
-				validation_fails_rdd = self.records_df.rdd.\
-					map(lambda row: udf_func(vs_id, vs_filepath, row))\
-					.filter(lambda row: row is not None)
+				validation_fails_rdd = self._sch_validation(vs, vs_id, vs_filepath)
 
 			# python based validation scenario
 			elif vs.validation_type == 'python':
-
-				# parse user defined functions from validation scenario payload
-				temp_pyvs = ModuleType('temp_pyvs')
-				exec(vs.payload, temp_pyvs.__dict__)
-
-				# get defined functions
-				pyvs_funcs = []
-				test_labeled_attrs = [ attr for attr in dir(temp_pyvs) if attr.lower().startswith('test') ]
-				for attr in test_labeled_attrs:
-					attr = getattr(temp_pyvs, attr)
-					if isfunction(attr):
-						pyvs_funcs.append(attr)
-
-				udf_func = self.validate_python_udf # must pass static method not attached to self				
-				validation_fails_rdd = self.records_df.rdd.\
-					map(lambda row: udf_func(vs_id, pyvs_funcs, row))\
-					.filter(lambda row: row is not None)
-
+				validation_fails_rdd = self._python_validation(vs, vs_id, vs_filepath)
 
 			# finally, write to DB if validation failures
 			if not validation_fails_rdd.isEmpty():
@@ -126,41 +109,124 @@ class ValidationScenarioSpark(object):
 					properties=settings.COMBINE_DATABASE,
 					mode='append')
 
+	
+	def _sch_validation(self, vs, vs_id, vs_filepath):
 
-	@staticmethod
-	def validate_schematron_udf(vs_id, vs_filepath, row):
+		self.logger.info('running schematron validation: %s' % vs.name)
+
+		def validate_schematron_pt_udf(pt):
 
 			# parse schematron
 			sct_doc = etree.parse(vs_filepath)
 			validator = isoschematron.Schematron(sct_doc, store_report=True)
 
-			# get document xml
-			record_xml = etree.fromstring(row.document.encode('utf-8'))
+			for row in pt:
 
-			# validate
-			is_valid = validator.validate(record_xml)
+				# get document xml
+				record_xml = etree.fromstring(row.document.encode('utf-8'))
 
-			# if not valid, prepare Row
-			if not is_valid:
+				# validate
+				is_valid = validator.validate(record_xml)
 
-				# prepare results_dict
-				results_dict = {
-					'fail_count':0,
-					'failed':[]
-				}
+				# if not valid, prepare Row
+				if not is_valid:
 
-				# get failed
-				report_root = validator.validation_report.getroot()
-				fails = report_root.findall('svrl:failed-assert', namespaces=report_root.nsmap)
+					# prepare results_dict
+					results_dict = {
+						'fail_count':0,
+						'failed':[]
+					}
 
-				# log fail_count
-				results_dict['fail_count'] = len(fails)
+					# get failed
+					report_root = validator.validation_report.getroot()
+					fails = report_root.findall('svrl:failed-assert', namespaces=report_root.nsmap)
 
-				# loop through fails and add to dictionary
-				for fail in fails:
-					fail_text_elem = fail.find('svrl:text', namespaces=fail.nsmap)
-					results_dict['failed'].append(fail_text_elem.text)
-				
+					# log fail_count
+					results_dict['fail_count'] = len(fails)
+
+					# loop through fails and add to dictionary
+					for fail in fails:
+						fail_text_elem = fail.find('svrl:text', namespaces=fail.nsmap)
+						results_dict['failed'].append(fail_text_elem.text)
+					
+					yield Row(
+						record_id=int(row.id),
+						validation_scenario_id=int(vs_id),
+						valid=0,
+						results_payload=json.dumps(results_dict),
+						fail_count=results_dict['fail_count']
+					)
+
+		# run pt_udf map
+		validation_fails_rdd = self.records_df.rdd.mapPartitions(validate_schematron_pt_udf).filter(lambda row: row is not None)
+
+		# return
+		return validation_fails_rdd
+
+
+	def _python_validation(self, vs, vs_id, vs_filepath):
+
+		self.logger.info('running python validation: %s' % vs.name)
+
+		def validate_python_udf(vs_id, pyvs_funcs, row):
+
+			'''
+			Loop through test functions and aggregate in fail_dict to return with Row
+
+			Args:
+				vs_id (int): integer of validation scenario
+				pyvs_funcs (list): list of functions imported from user created python validation scenario payload
+				row (): 
+			'''
+
+			# prepare row as parsed document with PythonUDFRecord class
+			prvb = PythonUDFRecord(row)
+			
+			# prepare results_dict
+			results_dict = {
+				'fail_count':0,
+				'failed':[]
+			}
+
+			# loop through functions
+			for func in pyvs_funcs:
+
+				# get name as string
+				func_name = func.__name__
+
+				# get func test message
+				func_signature = signature(func)
+				t_msg = func_signature.parameters['test_message'].default
+
+				# attempt to run user-defined validation function
+				try:
+
+					# run test
+					test_result = func(prvb)
+
+					# if fail, append
+					if test_result != True:
+
+						# bump fail count
+						results_dict['fail_count'] += 1
+
+						# if custom message override provided, use
+						if test_result != False:
+							results_dict['failed'].append(test_result)
+
+						# else, default to test message
+						else:
+							results_dict['failed'].append(t_msg)
+
+				# if problem, report as failure with Exception string
+				except Exception as e:
+					results_dict['fail_count'] += 1
+					results_dict['failed'].append("test '%s' had exception: %s" % (func_name, str(e)))
+
+			# if failed, return Row
+			if results_dict['fail_count'] > 0:
+
+				# return row			
 				return Row(
 					record_id=int(row.id),
 					validation_scenario_id=int(vs_id),
@@ -169,78 +235,24 @@ class ValidationScenarioSpark(object):
 					fail_count=results_dict['fail_count']
 				)
 
+		# parse user defined functions from validation scenario payload
+		temp_pyvs = ModuleType('temp_pyvs')
+		exec(vs.payload, temp_pyvs.__dict__)
 
-	@staticmethod
-	def validate_python_udf(vs_id, pyvs_funcs, row):
+		# get defined functions
+		pyvs_funcs = []
+		test_labeled_attrs = [ attr for attr in dir(temp_pyvs) if attr.lower().startswith('test') ]
+		for attr in test_labeled_attrs:
+			attr = getattr(temp_pyvs, attr)
+			if isfunction(attr):
+				pyvs_funcs.append(attr)
 
-		'''
-		Loop through test functions and aggregate in fail_dict to return with Row
+		validation_fails_rdd = self.records_df.rdd.\
+			map(lambda row: validate_python_udf(vs_id, pyvs_funcs, row))\
+			.filter(lambda row: row is not None)
 
-		Args:
-			vs_id (int): integer of validation scenario
-			pyvs_funcs (list): list of functions imported from user created python validation scenario payload
-			row (): 
-		'''
-
-		# prepare row as parsed document with PythonUDFRecord class
-		prvb = PythonUDFRecord(row)
-		
-		# prepare results_dict
-		results_dict = {
-			'fail_count':0,
-			'failed':[]
-		}
-
-		# loop through functions
-		for func in pyvs_funcs:
-
-			# get name as string
-			func_name = func.__name__
-
-			# get func test message
-			func_signature = signature(func)
-			t_msg = func_signature.parameters['test_message'].default
-
-			# attempt to run user-defined validation function
-			try:
-
-				# run test
-				test_result = func(prvb)
-
-				# if fail, append
-				if test_result != True:
-
-					# bump fail count
-					results_dict['fail_count'] += 1
-
-					# if custom message override provided, use
-					if test_result != False:
-						results_dict['failed'].append(test_result)
-
-					# else, default to test message
-					else:
-						results_dict['failed'].append(t_msg)
-
-			# if problem, report as failure with Exception string
-			except Exception as e:
-				results_dict['fail_count'] += 1
-				results_dict['failed'].append("test '%s' had exception: %s" % (func_name, str(e)))
-
-		# if failed, return Row
-		if results_dict['fail_count'] > 0:
-
-			# return row			
-			return Row(
-				record_id=int(row.id),
-				validation_scenario_id=int(vs_id),
-				valid=0,
-				results_payload=json.dumps(results_dict),
-				fail_count=results_dict['fail_count']
-			)
-
-
-
-
+		# return
+		return validation_fails_rdd
 
 
 
