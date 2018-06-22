8acafa41bcab1c10a6bd72ef80780bf3d6d2a44c
Graham Hukill <ghukill@gmail.com>
2018-06-22 23:22:53 +0000

    cleanup for percentage bars

--- core/models.py
+++ core/models.py
@@ -3137,8 +3137,13 @@ class ESIndex(object):
 			es_r = es_handle.indices.get(index=self.es_index)
 			self.index_mappings = es_r[self.es_index]['mappings']['record']['properties']
 
-			# sort alphabetically that influences results list
+			# get fields as list
 			field_names = list(self.index_mappings.keys())
+
+			# remove uninteresting fields
+			field_names = [ field for field in field_names if field not in ['db_id','combine_id'] ]
+
+			# sort alphabetically that influences results list
 			field_names.sort()
 
 			return field_names
