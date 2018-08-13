192b33d910e3f1e99cd9467f6f9238c10f4320c3
Graham Hukill <ghukill@gmail.com>
2018-08-13 11:59:32 +0000

    removing sql tables

--- core/inc/combine_add_fks.sql
+++
@@ -1,7 +0,0 @@
-
-/* 
-  Add forgeign keys for `core_record` and `core_indexmappingfailure`
-*/
-
-ALTER TABLE core_record ADD FOREIGN KEY (job_id) REFERENCES core_job(id) ON DELETE CASCADE;
-ALTER TABLE core_indexmappingfailure ADD FOREIGN KEY (job_id) REFERENCES core_job(id) ON DELETE CASCADE;
