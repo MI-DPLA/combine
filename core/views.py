220257a95bba1e72358603577330b7f02d749b50
Graham Hukill <ghukill@gmail.com>
2018-08-24 14:31:10 +0000

    searchable tables

--- core/views.py
+++ core/views.py
@@ -3340,45 +3340,24 @@ class DTDPLABulkDataMatches(BaseDatatableView):
 				return super(DTDPLABulkDataMatches, self).render_column(row, column)
 
 
-		# def get_context_data(self, *args, **kwargs):
-		# 	stime = time.time()
-		# 	try:
-		# 		self.initialize(*args, **kwargs)
+		def filter_queryset(self, qs):
+			# use parameters passed in GET request to filter queryset
 
-		# 		qs = self.get_initial_queryset()
+			# handle search
+			search = self.request.GET.get(u'search[value]', None)
+			if search:
+				# sniff out ObjectId if present
+				if len(search) == 24:
+					try:
+						oid = ObjectId(search)					
+						qs = qs.filter(mongoengine.Q(id=oid))
+					except:
+						logger.debug('recieved 24 chars, but not ObjectId')
+				else:
+					qs = qs.filter(mongoengine.Q(record_id=search))
 
-		# 		# number of records before filtering
-		# 		total_records = qs.count()
-
-		# 		qs = self.filter_queryset(qs)
-
-		# 		# number of records after filtering
-		# 		total_display_records = qs.count()
-
-		# 		qs = self.ordering(qs)
-		# 		qs = self.paging(qs)
-
-		# 		# prepare output data
-		# 		if self.pre_camel_case_notation:
-		# 			aaData = self.prepare_results(qs)
-
-		# 			ret = {'sEcho': int(self._querydict.get('sEcho', 0)),
-		# 				   'iTotalRecords': total_records,
-		# 				   'iTotalDisplayRecords': total_display_records,
-		# 				   'aaData': aaData
-		# 				   }
-		# 		else:
-		# 			data = self.prepare_results(qs)
-
-		# 			ret = {'draw': int(self._querydict.get('draw', 0)),
-		# 				   'recordsTotal': total_records,
-		# 				   'recordsFiltered': total_display_records,
-		# 				   'data': data
-		# 				   }
-		# 		logger.debug('context data total %s' % (time.time() - stime))
-		# 		return ret
-		# 	except Exception as e:
-		# 		return self.handle_exception(e)
+			# return
+			return qs
 
 
 
