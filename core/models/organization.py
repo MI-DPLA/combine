654ade5bfd6848dd30231f069282f1c3f6097e21
Esty Thomas <estelendur@gmail.com>
2019-06-10 15:29:27 -0400

    Linting organization

--- core/models/organization.py
+++ core/models/organization.py
@@ -7,11 +7,8 @@ import logging
 # django imports
 from django.db import models
 
-# import mongo dependencies
-from core.mongo import *
-
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
+# Get an instance of a LOGGER
+LOGGER = logging.getLogger(__name__)
 
 # Set logging levels for 3rd party modules
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -20,36 +17,36 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 
 class Organization(models.Model):
 
-	'''
-	Model to manage Organizations in Combine.
-	Organizations contain Record Groups, and are the highest level of organization in Combine.
-	'''
+    '''
+    Model to manage Organizations in Combine.
+    Organizations contain Record Groups, and are the highest level of organization in Combine.
+    '''
 
-	name = models.CharField(max_length=128)
-	description = models.CharField(max_length=255, blank=True)
-	timestamp = models.DateTimeField(null=True, auto_now_add=True)
-	for_analysis = models.BooleanField(default=0)
-
-
-	def __str__(self):
-		return 'Organization: %s' % self.name
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255, blank=True)
+    timestamp = models.DateTimeField(null=True, auto_now_add=True)
+    for_analysis = models.BooleanField(default=0)
 
 
-	def total_record_count(self):
+    def __str__(self):
+        return 'Organization: %s' % self.name
 
-		'''
-		Method to determine total records under this Org
-		'''
 
-		total_record_count = 0
+    def total_record_count(self):
 
-		# loop through record groups
-		for rg in self.recordgroup_set.all():
+        '''
+        Method to determine total records under this Org
+        '''
 
-			# loop through jobs
-			for job in rg.job_set.all():
+        total_record_count = 0
 
-				total_record_count += job.record_count
+        # loop through record groups
+        for record_group in self.recordgroup_set.all():
 
-		# return
-		return total_record_count
+            # loop through jobs
+            for job in record_group.job_set.all():
+
+                total_record_count += job.record_count
+
+        # return
+        return total_record_count
