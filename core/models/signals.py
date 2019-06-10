bf4374e999b287674f71f8e206335fe616715efa
Esty Thomas <estelendur@gmail.com>
2019-06-10 15:49:46 -0400

    Linting signals

--- core/models/signals.py
+++ core/models/signals.py
@@ -10,319 +10,311 @@ import uuid
 # django imports
 from django.conf import settings
 from django.contrib.auth import signals
-from django.db import connection, models, transaction
+from django.db import models
 from django.dispatch import receiver
 
-# import elasticsearch and handles
-from core.es import es_handle
-
-# import mongo dependencies
-from core.mongo import *
-
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
+# Get an instance of a LOGGER
+LOGGER = logging.getLogger(__name__)
 
 # Set logging levels for 3rd party modules
 logging.getLogger("requests").setLevel(logging.WARNING)
 
-# core models imports
-from core.models.configurations import OAIEndpoint, Transformation, ValidationScenario, FieldMapper,\
-    RecordIdentifierTransformationScenario, DPLABulkDataDownload
-from core.models.job import Job, IndexMappingFailure, JobValidation, JobTrack, JobInput, CombineJob, HarvestJob,\
-	HarvestOAIJob, HarvestStaticXMLJob, HarvestTabularDataJob, TransformJob, MergeJob, AnalysisJob
-from core.models.livy_spark import LivySession, LivyClient, SparkAppAPIClient
+from core.es import es_handle
+from core.mongo import mongoengine
+from core.models.configurations import Transformation, ValidationScenario, DPLABulkDataDownload
+from core.models.job import Job, JobValidation
+from core.models.livy_spark import LivySession
 from core.models.organization import Organization
 from core.models.record_group import RecordGroup
-from core.models.stateio import StateIO, StateIOClient
+from core.models.stateio import StateIO
 from core.models.tasks import CombineBackgroundTask
 
 
 
 @receiver(signals.user_logged_in)
-def user_login_handle_livy_sessions(sender, user, **kwargs):
+def user_login_handle_livy_sessions(_sender, user, **_kwargs):
 
-	'''
-	When user logs in, handle check for pre-existing sessions or creating
+    '''
+    When user logs in, handle check for pre-existing sessions or creating
 
-	Args:
-		sender (auth.models.User): class
-		user (auth.models.User): instance
-		kwargs: not used
-	'''
+    Args:
+        sender (auth.models.User): class
+        user (auth.models.User): instance
+        kwargs: not used
+    '''
 
-	# if superuser, skip
-	if user.is_superuser:
-		logger.debug("superuser detected, not initiating Livy session")
-		return False
+    # if superuser, skip
+    if user.is_superuser:
+        LOGGER.debug("superuser detected, not initiating Livy session")
+        return False
 
-	# else, continune with user sessions
-	else:
-		logger.debug('Checking for pre-existing livy sessions')
+    # else, continune with user sessions
+    LOGGER.debug('Checking for pre-existing livy sessions')
 
-		# get "active" user sessions
-		livy_sessions = LivySession.objects.filter(status__in=['starting','running','idle'])
-		logger.debug(livy_sessions)
+    # get "active" user sessions
+    livy_sessions = LivySession.objects.filter(status__in=['starting', 'running', 'idle'])
+    LOGGER.debug(livy_sessions)
 
-		# none found
-		if livy_sessions.count() == 0:
-			logger.debug('no Livy sessions found, creating')
-			livy_session = models.LivySession()
-			livy_session.start_session()
+    # none found
+    if livy_sessions.count() == 0:
+        LOGGER.debug('no Livy sessions found, creating')
+        livy_session = LivySession()
+        livy_session.start_session()
 
-		# if sessions present
-		elif livy_sessions.count() == 1:
-			logger.debug('single, active Livy session found, using')
+    # if sessions present
+    elif livy_sessions.count() == 1:
+        LOGGER.debug('single, active Livy session found, using')
 
-		elif livy_sessions.count() > 1:
-			logger.debug('multiple Livy sessions found, sending to sessions page to select one')
+    elif livy_sessions.count() > 1:
+        LOGGER.debug('multiple Livy sessions found, sending to sessions page to select one')
 
 
 @receiver(models.signals.pre_delete, sender=Organization)
-def delete_org_pre_delete(sender, instance, **kwargs):
+def delete_org_pre_delete(_sender, instance, **_kwargs):
 
-	# mark child record groups as deleted
-	logger.debug('marking all child Record Groups as deleting')
-	for record_group in instance.recordgroup_set.all():
+    # mark child record groups as deleted
+    LOGGER.debug('marking all child Record Groups as deleting')
+    for record_group in instance.recordgroup_set.all():
 
-		record_group.name = "%s (DELETING)" % record_group.name
-		record_group.save()
+        record_group.name = "%s (DELETING)" % record_group.name
+        record_group.save()
 
-		# mark child jobs as deleted
-		logger.debug('marking all child Jobs as deleting')
-		for job in record_group.job_set.all():
+        # mark child jobs as deleted
+        LOGGER.debug('marking all child Jobs as deleting')
+        for job in record_group.job_set.all():
 
-			job.name = "%s (DELETING)" % job.name
-			job.deleted = True
-			job.status = 'deleting'
-			job.save()
+            job.name = "%s (DELETING)" % job.name
+            job.deleted = True
+            job.status = 'deleting'
+            job.save()
 
 
 @receiver(models.signals.pre_delete, sender=RecordGroup)
-def delete_record_group_pre_delete(sender, instance, **kwargs):
+def delete_record_group_pre_delete(_sender, instance, **_kwargs):
 
-	# mark child jobs as deleted
-	logger.debug('marking all child Jobs as deleting')
-	for job in instance.job_set.all():
+    # mark child jobs as deleted
+    LOGGER.debug('marking all child Jobs as deleting')
+    for job in instance.job_set.all():
 
-		job.name = "%s (DELETING)" % job.name
-		job.deleted = True
-		job.status = 'deleting'
-		job.save()
+        job.name = "%s (DELETING)" % job.name
+        job.deleted = True
+        job.status = 'deleting'
+        job.save()
 
 
 @receiver(models.signals.post_save, sender=Job)
-def save_job_post_save(sender, instance, created, **kwargs):
+def save_job_post_save(_sender, instance, created, **_kwargs):
 
-	'''
-	After job is saved, update job output
+    '''
+    After job is saved, update job output
 
-	Args:
-		sender (auth.models.Job): class
-		user (auth.models.Job): instance
-		created (bool): indicates if newly created, or just save/update
-		kwargs: not used
-	'''
+    Args:
+        sender (auth.models.Job): class
+        user (auth.models.Job): instance
+        created (bool): indicates if newly created, or just save/update
+        kwargs: not used
+    '''
 
-	# if the record was just created, then update job output (ensures this only runs once)
-	if created and instance.job_type != 'AnalysisJob':
+    # if the record was just created, then update job output (ensures this only runs once)
+    if created and instance.job_type != 'AnalysisJob':
 
-		# set output based on job type
-		logger.debug('setting job output for job')
-		instance.job_output = '%s/organizations/%s/record_group/%s/jobs/%s/%s' % (
-			settings.BINARY_STORAGE.rstrip('/'),
-			instance.record_group.organization.id,
-			instance.record_group.id,
-			instance.job_type,
-			instance.id)
-		instance.save()
+        # set output based on job type
+        LOGGER.debug('setting job output for job')
+        instance.job_output = '%s/organizations/%s/record_group/%s/jobs/%s/%s' % (
+            settings.BINARY_STORAGE.rstrip('/'),
+            instance.record_group.organization.id,
+            instance.record_group.id,
+            instance.job_type,
+            instance.id)
+        instance.save()
 
 
 @receiver(models.signals.pre_delete, sender=Job)
-def delete_job_pre_delete(sender, instance, **kwargs):
+def delete_job_pre_delete(_sender, instance, **_kwargs):
 
-	'''
-	When jobs are removed, some actions are performed:
-		- if job is queued or running, stop
-		- remove avro files from disk
-		- delete ES indexes (if present)
-		- delete from Mongo
+    '''
+    When jobs are removed, some actions are performed:
+        - if job is queued or running, stop
+        - remove avro files from disk
+        - delete ES indexes (if present)
+        - delete from Mongo
 
-	Args:
-		sender (auth.models.Job): class
-		user (auth.models.Job): instance
-		kwargs: not used
-	'''
+    Args:
+        sender (auth.models.Job): class
+        user (auth.models.Job): instance
+        kwargs: not used
+    '''
 
-	logger.debug('pre-delete sequence for Job: #%s' % instance.id)
+    LOGGER.debug('pre-delete sequence for Job: #%s', instance.id)
 
-	# stop Job
-	instance.stop_job()
+    # stop Job
+    instance.stop_job()
 
-	# remove avro files from disk
-	if instance.job_output and instance.job_output.startswith('file://'):
+    # remove avro files from disk
+    if instance.job_output and instance.job_output.startswith('file://'):
 
-		try:
-			output_dir = instance.job_output.split('file://')[-1]
-			shutil.rmtree(output_dir)
+        try:
+            output_dir = instance.job_output.split('file://')[-1]
+            shutil.rmtree(output_dir)
 
-		except:
-			logger.debug('could not remove job output directory at: %s' % instance.job_output)
+        except:
+            LOGGER.debug('could not remove job output directory at: %s', instance.job_output)
 
-	# remove ES index if exists
-	instance.drop_es_index()
+    # remove ES index if exists
+    instance.drop_es_index()
 
-	# remove Records from Mongo
-	instance.remove_records_from_db()
+    # remove Records from Mongo
+    instance.remove_records_from_db()
 
-	# remove Validations from Mongo
-	instance.remove_validations_from_db()
+    # remove Validations from Mongo
+    instance.remove_validations_from_db()
 
-	# remove Validations from Mongo
-	instance.remove_mapping_failures_from_db()
+    # remove Validations from Mongo
+    instance.remove_mapping_failures_from_db()
 
-	# remove Job as input for other Jobs
-	instance.remove_as_input_job()
+    # remove Job as input for other Jobs
+    instance.remove_as_input_job()
 
-	# if Job published, remove pre-counts where necessary
-	instance.remove_from_published_precounts()
+    # if Job published, remove pre-counts where necessary
+    instance.remove_from_published_precounts()
 
-	# remove any temporary files
-	instance.remove_temporary_files()
+    # remove any temporary files
+    instance.remove_temporary_files()
 
 
 @receiver(models.signals.pre_delete, sender=JobValidation)
-def delete_job_validation_pre_delete(sender, instance, **kwargs):
+def delete_job_validation_pre_delete(_sender, instance, **_kwargs):
 
-	'''
-	Signal to remove RecordValidations from DB if JobValidation removed
-	'''
+    '''
+    Signal to remove RecordValidations from DB if JobValidation removed
+    '''
 
-	del_results = instance.delete_record_validation_failures()
+    _ = instance.delete_record_validation_failures()
 
 
 @receiver(models.signals.post_delete, sender=Job)
-def delete_job_post_delete(sender, instance, **kwargs):
+def delete_job_post_delete(_sender, instance, **_kwargs):
 
-	logger.debug('job %s was deleted successfully' % instance)
+    LOGGER.debug('job %s was deleted successfully', instance)
 
 
 @receiver(models.signals.pre_save, sender=Transformation)
-def save_transformation_to_disk(sender, instance, **kwargs):
+def save_transformation_to_disk(_sender, instance, **_kwargs):
 
-	'''
-	Pre-save work for Transformations
+    '''
+    Pre-save work for Transformations
 
-	Args:
-		sender (auth.models.Transformation): class
-		user (auth.models.Transformation): instance
-		kwargs: not used
-	'''
+    Args:
+        sender (auth.models.Transformation): class
+        user (auth.models.Transformation): instance
+        kwargs: not used
+    '''
 
-	# check that transformation directory exists
-	transformations_dir = '%s/transformations' % settings.BINARY_STORAGE.rstrip('/').split('file://')[-1]
-	if not os.path.exists(transformations_dir):
-		os.mkdir(transformations_dir)
+    # check that transformation directory exists
+    transformations_dir = '%s/transformations' % settings.BINARY_STORAGE.rstrip('/').split('file://')[-1]
+    if not os.path.exists(transformations_dir):
+        os.mkdir(transformations_dir)
 
-	# if previously written to disk, remove
-	if instance.filepath:
-		try:
-			os.remove(instance.filepath)
-		except:
-			logger.debug('could not remove transformation file: %s' % instance.filepath)
+    # if previously written to disk, remove
+    if instance.filepath:
+        try:
+            os.remove(instance.filepath)
+        except:
+            LOGGER.debug('could not remove transformation file: %s', instance.filepath)
 
-	# fire transformation method to rewrite external HTTP includes for XSLT
-	if instance.transformation_type == 'xslt':
-		instance._rewrite_xsl_http_includes()
+    # fire transformation method to rewrite external HTTP includes for XSLT
+    if instance.transformation_type == 'xslt':
+        instance._rewrite_xsl_http_includes()
 
-	# write XSLT type transformation to disk
-	if instance.transformation_type == 'xslt':
-		filename = uuid.uuid4().hex
+    # write XSLT type transformation to disk
+    if instance.transformation_type == 'xslt':
+        filename = uuid.uuid4().hex
 
-		filepath = '%s/%s.xsl' % (transformations_dir, filename)
-		with open(filepath, 'w') as f:
-			f.write(instance.payload)
+        filepath = '%s/%s.xsl' % (transformations_dir, filename)
+        with open(filepath, 'w') as outfile:
+            outfile.write(instance.payload)
 
-		# update filepath
-		instance.filepath = filepath
+        # update filepath
+        instance.filepath = filepath
 
 
 @receiver(models.signals.pre_save, sender=ValidationScenario)
-def save_validation_scenario_to_disk(sender, instance, **kwargs):
+def save_validation_scenario_to_disk(_sender, instance, **_kwargs):
 
-	'''
-	When users enter a payload for a validation scenario, write to disk for use in Spark context
+    '''
+    When users enter a payload for a validation scenario, write to disk for use in Spark context
 
-	Args:
-		sender (auth.models.ValidationScenario): class
-		user (auth.models.ValidationScenario): instance
-		kwargs: not used
-	'''
+    Args:
+        sender (auth.models.ValidationScenario): class
+        user (auth.models.ValidationScenario): instance
+        kwargs: not used
+    '''
 
-	# check that transformation directory exists
-	validations_dir = '%s/validation' % settings.BINARY_STORAGE.rstrip('/').split('file://')[-1]
-	if not os.path.exists(validations_dir):
-		os.mkdir(validations_dir)
+    # check that transformation directory exists
+    validations_dir = '%s/validation' % settings.BINARY_STORAGE.rstrip('/').split('file://')[-1]
+    if not os.path.exists(validations_dir):
+        os.mkdir(validations_dir)
 
-	# if previously written to disk, remove
-	if instance.filepath:
-		try:
-			os.remove(instance.filepath)
-		except:
-			logger.debug('could not remove validation scenario file: %s' % instance.filepath)
+    # if previously written to disk, remove
+    if instance.filepath:
+        try:
+            os.remove(instance.filepath)
+        except:
+            LOGGER.debug('could not remove validation scenario file: %s', instance.filepath)
 
-	# write Schematron type validation to disk
-	if instance.validation_type == 'sch':
-		filename = 'file_%s.sch' % uuid.uuid4().hex
-	if instance.validation_type == 'python':
-		filename = 'file_%s.py' % uuid.uuid4().hex
-	if instance.validation_type == 'es_query':
-		filename = 'file_%s.json' % uuid.uuid4().hex
-	if instance.validation_type == 'xsd':
-		filename = 'file_%s.xsd' % uuid.uuid4().hex
+    # write Schematron type validation to disk
+    if instance.validation_type == 'sch':
+        filename = 'file_%s.sch' % uuid.uuid4().hex
+    if instance.validation_type == 'python':
+        filename = 'file_%s.py' % uuid.uuid4().hex
+    if instance.validation_type == 'es_query':
+        filename = 'file_%s.json' % uuid.uuid4().hex
+    if instance.validation_type == 'xsd':
+        filename = 'file_%s.xsd' % uuid.uuid4().hex
 
-	filepath = '%s/%s' % (validations_dir, filename)
-	with open(filepath, 'w') as f:
-		f.write(instance.payload)
+    filepath = '%s/%s' % (validations_dir, filename)
+    with open(filepath, 'w') as out_file:
+        out_file.write(instance.payload)
 
-	# update filepath
-	instance.filepath = filepath
+    # update filepath
+    instance.filepath = filepath
 
 
 @receiver(models.signals.pre_delete, sender=DPLABulkDataDownload)
-def delete_dbdd_pre_delete(sender, instance, **kwargs):
+def delete_dbdd_pre_delete(_sender, instance, **_kwargs):
 
-	# remove download from disk
-	if os.path.exists(instance.filepath):
-		logger.debug('removing %s from disk' % instance.filepath)
-		os.remove(instance.filepath)
+    # remove download from disk
+    if os.path.exists(instance.filepath):
+        LOGGER.debug('removing %s from disk', instance.filepath)
+        os.remove(instance.filepath)
 
-	# remove ES index if exists
-	try:
-		if es_handle.indices.exists(instance.es_index):
-			logger.debug('removing ES index: %s' % instance.es_index)
-			es_handle.indices.delete(instance.es_index)
-	except:
-		logger.debug('could not remove ES index: %s' % instance.es_index)
+    # remove ES index if exists
+    try:
+        if es_handle.indices.exists(instance.es_index):
+            LOGGER.debug('removing ES index: %s', instance.es_index)
+            es_handle.indices.delete(instance.es_index)
+    except:
+        LOGGER.debug('could not remove ES index: %s', instance.es_index)
 
 
 @receiver(models.signals.post_init, sender=CombineBackgroundTask)
-def background_task_post_init(sender, instance, **kwargs):
+def background_task_post_init(_sender, instance, **_kwargs):
 
-	# if exists already, update status
-	if instance.id:
-		instance.update()
+    # if exists already, update status
+    if instance.id:
+        instance.update()
 
 
 @receiver(models.signals.pre_delete, sender=CombineBackgroundTask)
-def background_task_pre_delete_django_tasks(sender, instance, **kwargs):
+def background_task_pre_delete_django_tasks(_sender, instance, **_kwargs):
 
-	# if export dir exists in task_output, delete as well
-	if instance.task_output != {} and 'export_dir' in instance.task_output.keys():
-		try:
-			logger.debug('removing task export dir: %s' % instance.task_output['export_dir'])
-			shutil.rmtree(instance.task_output['export_dir'])
-		except:
-			logger.debug('could not parse task output as JSON')
+    # if export dir exists in task_output, delete as well
+    if instance.task_output != {} and 'export_dir' in instance.task_output.keys():
+        try:
+            LOGGER.debug('removing task export dir: %s', instance.task_output['export_dir'])
+            shutil.rmtree(instance.task_output['export_dir'])
+        except:
+            LOGGER.debug('could not parse task output as JSON')
 
 
 # MongoEngine signals
