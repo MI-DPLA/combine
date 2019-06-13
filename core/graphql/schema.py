c3ae137591163425ac786c11888d5db894263eff
Graham Hukill <ghukill@gmail.com>
2019-06-13 09:18:45 -0400

    establishing linkage to mapped fields

--- core/graphql/schema.py
+++ core/graphql/schema.py
@@ -1,3 +1,8 @@
+
+# generic imports
+import json
+import logging
+
 # graphene imports
 import graphene
 from graphene import ObjectType, String, Schema
@@ -11,6 +16,72 @@ from graphene_mongo import MongoengineObjectType
 # core imports
 from core.models import Organization, RecordGroup, Job, Record
 
+# Get an instance of a LOGGER
+LOGGER = logging.getLogger(__name__)
+
+# Set logging levels for 3rd party modules
+logging.getLogger("requests").setLevel(logging.WARNING)
+
+
+class RecordMappedFields(ObjectType):
+
+    '''
+    Returning the unknown fields that result from Record mapping is currently not possible:
+    https://github.com/graphql/graphql-spec/issues/101
+
+    Possible workaround would be returning string of ES document (see mapped_fields_json below)
+
+    Another option would be query defined aliases and mapping, a la:
+    https://github.com/graphql/graphql-spec/issues/101#issuecomment-170170967
+        - a query could ping the ES index and retrieve all fields, looping through and aliasing
+        - see mapped_field below
+        - could potentially ping Job for all fields as part of the
+        CombineJob.mapped_fields_analysis() method
+
+        example query:
+        query {
+          all_records(first:100) {
+            id
+            abstract:mapped_field(field_name:"mods_abstract") {
+              value
+            }
+            subjects:mapped_field(field_name:"mods_subject_topic") {
+              value
+            }
+          }
+        }
+
+        This is clearly not optimal, as each mapped_field that is requested in the graphql query, for each Record,
+        is a separate request to ES
+    '''
+
+    record_id = String()
+    mods_subject_topic = String()
+
+
+class RecordType(MongoengineObjectType):
+
+    class Meta:
+        model = Record
+
+    mapped_fields = graphene.Field(RecordMappedFields)
+
+    def resolve_mapped_fields(instance, info, **kwargs):
+        return instance.get_es_doc()
+
+    mapped_fields_json = graphene.Field(graphene.String)
+
+    def resolve_mapped_fields_json(instance, info, **kwargs):
+        return json.dumps(instance.get_es_doc())
+
+    mapped_field = graphene.List(graphene.String, field_name=graphene.String())
+    def resolve_mapped_field(instance, info, **kwargs):
+        field_value = instance.get_es_doc().get(kwargs.get('field_name'))
+        if type(field_value) == list:
+            return [value for value in field_value]
+        else:
+            return [field_value]
+
 
 class OrganizationType(DjangoObjectType):
     class Meta:
@@ -23,13 +94,18 @@ class RecordGroupType(DjangoObjectType):
 
 
 class JobType(DjangoObjectType):
+
     class Meta:
         model = Job
 
+    all_records = graphene.List(RecordType, first=graphene.Int())
+    def resolve_all_records(instance, info, **kwargs):
 
-class RecordType(MongoengineObjectType):
-    class Meta:
-        model = Record
+        first = kwargs.get('first')
+
+        if first is not None:
+            return instance.get_records()[:int(first)]
+        return instance.get_records()
 
 
 class Query(graphene.ObjectType):
@@ -45,6 +121,9 @@ class Query(graphene.ObjectType):
     all_record_groups = graphene.List(RecordGroupType)
 
     # Job
+    job = graphene.Field(JobType,
+                         id=graphene.Int()
+                         )
     all_jobs = graphene.List(JobType,
                              first=graphene.Int()
                              )
@@ -73,6 +152,15 @@ class Query(graphene.ObjectType):
     def resolve_all_record_groups(self, info, **kwargs):
         return RecordGroup.objects.select_related('organization').all()
 
+    def resolve_job(self, info, **kwargs):
+
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Job.objects.get(pk=id)
+
+        return None
+
     def resolve_all_jobs(self, info, **kwargs):
 
         first = kwargs.get('first')
@@ -92,38 +180,6 @@ class Query(graphene.ObjectType):
             return Record.objects.all()[:int(first)]
 
         return Record.objects.all()
-
-
-    ####################################################################################################
-    # ElasticSearch exp.
-    ####################################################################################################
-
-    class RecordMappedFields(ObjectType):
-        record_id = String()
-        mods_extension_bibNo = String()
-
-    # # Record Mapped Fields
-    # all_record_mapped_fields = graphene.List(RecordMappedFields)
-    #
-    # def resolve_all_record_mapped_fields(self, info, **kwargs):
-    #
-    #     return [ {'record_id':'abc%s' % x} for x in range(0,20) ]
-
-    record_mapped_fields= graphene.Field(RecordMappedFields,
-                                  id=graphene.String(),
-                                  )
-
-    def resolve_record_mapped_fields(self, info, **kwargs):
-
-        id = kwargs.get('id')
-
-        # get Record
-        record = Record.objects.get(id=id)
-
-        # return Record' mapped fields as ES doc
-        return record.get_es_doc()
-
-    ####################################################################################################
 
 
 # return schema
