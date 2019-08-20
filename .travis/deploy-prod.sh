f17ffc4d51fe20491b73f2e042fdc977cd7a1df5
Christina Harlow <cmharlow@gmail.com>
2019-08-20 15:56:17 -0400

    adding CI/CD setup with travis

---
+++ .travis/deploy-prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd ..
+git clone --single-branch --branch master git@github.com:tulibraries/ansible-playbook-combine-docker.git # clone deployment playbook
+cd ansible-playbook-combine-docker
+sudo pip install pipenv # install playbook requirements
+pipenv install # install playbook requirements
+pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
+cp .circleci/.vault ~/.vault # setup vault password retrieval from travis envvar
+chmod +x ~/.vault  # setup vault password retrieval from travis envvar
+
+# deploy to qa using ansible-playbook
+pipenv run ansible-playbook -i inventory/prod/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private-key=~/.ssh/.conan_the_deployer
