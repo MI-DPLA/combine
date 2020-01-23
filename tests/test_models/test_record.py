c1a26b4323e5218d4dba79d614c063b74bb985a7
Esty Thomas <esty@umich.edu>
2020-01-23 11:01:51 -0500

    Fix a small test

--- tests/test_models/test_record.py
+++ tests/test_models/test_record.py
@@ -7,4 +7,4 @@ class RecordModelTestCase(TestCase):
     def test_record_with_nonexistent_job(self):
         record = Record(job_id=2345789)
         self.assertEqual(record.job.name, 'No Job')
-        self.assertEqual(record.job.record_group, 'No Record Group')
+        self.assertEqual(record.job.record_group.name, 'No Record Group')
